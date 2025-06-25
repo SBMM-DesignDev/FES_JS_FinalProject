26e96d0a759932c2b2cfc1140a8264dabe6a578b
Sherman <sbenton.multimedia@gmail.com>
2025-06-24 21:23:09 -0500

    Review code index.js for dynamic code for innerHTML

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 
+
+
+
 async function movieSearch() {
-const movies = await fetch('https://omdbapi.com/?apikey=242fafd7&=s');
-const moviesData = await movies.json();
-console.log(moviesData);
+    // wokring url https://omdbapi.com/?apikey=242fafd7&s=ice
+    const movies = await fetch('https://omdbapi.com/?apikey=242fafd7&s=ice');
+    const moviesData = await movies.json();
+    const moviesHTML = document.querySelector('.movie__search--list');
+   console.log(moviesData)
+   
+     moviesHTML.innerHTML = Object.keys(moviesData).map(
+        (movie) => `<div class="movie-card">
+        <div class="movie-card__container">
+            <div class="movie-poster"></div>
+            <div class="movie-poster__info">
+                <h3 class=${movie.title}></h3>
+                <h5 class=${movie.year}></h5>
+                <p class="movie-theatre">IMAX Cinema</p>
+                <p class="movie-theatre__location">Movie City, USA</p>
+                <p class="movie-theatre--phone">(800)MOV-IE11</p>
+            </div>
+        </div>
+    </div>`
+   
+   .join('')
+);
 }
 
 movieSearch();
