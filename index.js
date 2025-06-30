fea96cf7ad3ad0ee3741649a22c44e963d9597f2
Sherman <sbenton.multimedia@gmail.com>
2025-06-30 11:21:27 -0500

    Reviewing search bar input value and JSredirect

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 
 
-
+const moviesHTML = document.querySelector('.movie__search--list');
+const userInput = document.querySelector('.input__movie--search')
 
 async function movieSearch() {
     // wokring url https://omdbapi.com/?apikey=242fafd7&s=ice
-    const movies = await fetch('https://omdbapi.com/?apikey=242fafd7&s=ice');
+    const movies = await fetch(`https://omdbapi.com/?apikey=242fafd7&s=${userInput}`);
     const moviesData = await movies.json();
-    const moviesHTML = document.querySelector('.movie__search--list');
-   console.log(moviesData)
+    console.log(moviesData)
+    moviesHTML.innerHTML = moviesData.Search.map((movie) => userHTML(movie)).join("");
+}
+
+movieSearch();
+
+function showMovieInfo(userInput) {
+    localStorage.setItem("id", userInput);
+    window.location.href = `${window.location.origin}/movies.html`
    
-     moviesHTML.innerHTML = Object.keys(moviesData).map(
-        (movie) => `<div class="movie-card">
+}
+
+
+function userHTML(movie) {
+    return `<div class="movie-card" )">
         <div class="movie-card__container">
             <div class="movie-poster"></div>
             <div class="movie-poster__info">
-                <h3 class=${movie.title}></h3>
-                <h5 class=${movie.year}></h5>
+                <h3 class="movie-title">${movie.Title}</h3>
+                <h5 class="movie-year">${movie.Year}</h5>
                 <p class="movie-theatre">IMAX Cinema</p>
                 <p class="movie-theatre__location">Movie City, USA</p>
                 <p class="movie-theatre--phone">(800)MOV-IE11</p>
             </div>
         </div>
-    </div>`
-   
-   .join('')
-);
+    </div>`;
 }
 
-movieSearch();
+
