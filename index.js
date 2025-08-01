5ac3f097751f92e0c1cea79ee9ae3efe0f6bb8d3
Sherman <sbenton.multimedia@gmail.com>
2025-07-31 22:42:56 -0500

    undefined value with onchange renderMovies( )

--- index.js
+++ index.js
@@ -3,13 +3,10 @@
 const moviesHTML = document.querySelector('.movie__search--list');
 const userInput = document.querySelector('.input__movie--search');
 const keyWord = document.querySelector('.search-by');
-const movieScreen = document.querySelector('.row__img');
 const sortMovies = document.querySelector('.sort__movies');
-const sort = movieSort();
-const movieQuery = movieResults();
 
-async function movieSearch(query, sortSearch) {
-   //movieScreen.classList.remove('.row__img');
+
+async function movieSearch(query) {
     //const loading = document.querySelector('.loading');
     //const results = document.querySelector('.movie__search--list');
     moviesHTML.classList += " movies__loading";
@@ -19,76 +16,36 @@ async function movieSearch(query, sortSearch) {
     // wokring url https://omdbapi.com/?apikey=242fafd7&s=ice
     const movies = await fetch(`https://omdbapi.com/?apikey=242fafd7&s=${query}`);
     const moviesData = await movies.json();
-    moviesData.Search.length = 6;
-    console.log(moviesData);
+     moviesData.Search.length = 6;
+    //console.log(moviesData.Search);
     moviesHTML.innerHTML = moviesData.Search.map((movie) => userHTML(movie)).join("");
 
      moviesHTML.classList.remove('movies__loading')
 
      keyWord.innerHTML = `<p class="key-word">Search Keyword "${query}"</p>`
 
-    if (sortSearch === "LATEST") {
-        moviesData.sort((a,b) => b.Year - a.Year);
-        
-     }
-     else if (sortSearch === "EARLIEST") {
-        moviesData.sort((a,b) => a.Year - b.Year);
-        
-     }
+    
 
-     sortMovies.innerHTML = `<select id="filter" onchange="movieSort(event.target.value)"> 
+     sortMovies.innerHTML = `<select id="filter" onchange="movieSort(event)"> 
                                 <option value="" disabled selected>Sort</option>
                                 <option value="LATEST">Latest Release</option>
                                 <option value="EARLIEST">Earliest Release</option>
                             </select>`
-}
 
-function movieSort(sorted) {
-   
-//    document.getElementById('filter').addEventListener('onchange', (event) => {
-//         const sort = event.target.value;
-        
-//          movieSearch(event, sort);
-//          return
-        
-//    });
-         
-    //      if (sorted === "LATEST") {
-    //     console.log(sorted)
-    // }
-    // else if (sorted === "EARLIEST") {
-    //     console.log(sorted)
-    // }
-        
-     return movieSearch(sorted);
-   
+               return moviesData.Search;          
 };
 
 
-
-
-
-
-
-
+/////////////////////////////////
 
 
 function movieResults() {
     
-    
-    event.preventDefault();
-    
-   return movieSearch(userInput.value);
-
+    movieSearch(userInput.value);
 }
 
 
-
-/*function showMovieInfo(userInput) {
-    localStorage.setItem("id", userInput);
-    window.location.href = `${window.location.origin}/movies.html`
-   
-}*/
+//////////////////////////////////////////
 
 
 function userHTML(movie) {
@@ -111,5 +68,62 @@ function userHTML(movie) {
         </div>
     </div>`;
 }
+
+/////////////////////////////////////////
+
+async function renderMovies(filter) {
+
+     arrayAPI = await movieSearch();
+
+     if (filter === "LATEST") {
+        arrayAPI.sort((a,b) => b.Year - a.Year);
+     }
+     else if (filter === "EARLIEST") {
+        arrayAPI.sort((a,b) => a.Year - b.Year);
+     }
+
+
+const moviesFilterResults = arrayAPI
+    .map((movie) => {
+    return `
+    <div class="movie-card" )">
+        
+        <div class="movie-card__container">
+            <div class="movie-poster">
+                <img class="movie-poster--img" src="${movie.Poster}" alt="movie poster">
+            </div>
+            <div class="movie-poster__info">
+                <h3 class="movie-title">${movie.Title}</h3>
+                <h5 class="movie-year">${movie.Year}</h5>
+                <p class="movie-theatre">IMAX Cinema</p>
+                <p class="movie-theatre__location">Movie City, USA</p>
+                <p class="movie-theatre--phone">(800)MOV-IE11</p>
+            </div>
+        </div>
+    </div>`;
+        })
+        .join("");
+
+    moviesHTML.innerHTML = moviesFilterResults
+
+}
+
+//////////////////////////////////////////////
+
+function movieSort(event) {
+   
+//    document.getElementById('filter').addEventListener('onchange', (event) => {
+//         const sort = event.target.value;
+        
+//          movieSearch(event, sort);
+//          return
+        
+//    });
+        
+        renderMovies(event.target.value);
+}
+
+
+
 
 
